b30d6b599ac74b151a74c66ec2110f88146c2107
sbx_user1051 <sbx_user1051@169.254.104.29>
2024-09-05 13:58:44 +0000

    Thu Sep 05 13:58:44 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,11 +5,23 @@ from collections import defaultdict
 def checkout(skus):
     prices = {"A": 50, "B": 30, "C": 20, "D": 15}
     offers = {"A": (3, 130), "B": (2, 45)}
-    item_counts = {}
+    item_counts = defaultdict(int)
 
     for sku in skus:
         if sku not in prices:
             return -1
+        else:
+            item_counts[sku] += 1
 
     total = 0
+    for item, counts in item_counts.items():
+        offer = offers.get(item)
+        if offer:
+            offer_count, offer_price = offer
+            total += (counts // offer_count) * offer_price
+            counts %= offer_count
+        total += counts * prices[item]
+    
+    return total
+
 
