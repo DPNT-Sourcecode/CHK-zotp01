2d446261c42dc5134648e4bee895487f45c12641
sbx_user1051 <sbx_user1051@169.254.104.29>
2024-09-05 15:26:45 +0000

    Thu Sep 05 15:26:45 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,12 +23,13 @@ def checkout(skus: str) -> int:
 
         # calculate the free items to be removed from the total after the financial offers
         if item_extra_item_offers:
+            temp_count = count
             # apply the best offer first and then work down
             sorted_special_offers = sorted(item_extra_item_offers, key=lambda x: -x[0])
             for offer_count, offer_item in sorted_special_offers:
-                free_item_count = count // offer_count
+                free_item_count = temp_count // offer_count
                 free_items[offer_item] += free_item_count
-                count %= offer_count
+                temp_count %= offer_count
 
         if item_offers:
             # apply the best offer first and then work down
