5e04ab64af02b08f19a60a07650a36c8a1f31b7d
sbx_user1051 <sbx_user1051@169.254.47.141>
2024-09-06 15:10:10 +0000

    Fri Sep 06 15:10:10 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -16,6 +16,7 @@ class TestCheckout():
         assert checkout_solution.checkout("AAABBBC123@") == -1
         # test case with extra item offers
         assert checkout_solution.checkout("EEB") == 80
+        assert checkout_solution.checkout("E") == 80
         # test case with extra item offers and financial offers
         # should get one B free after the 2 for 45 B offer
         assert checkout_solution.checkout("EEBB") == 110
@@ -31,3 +32,4 @@ class TestCheckout():
         assert checkout_solution.checkout("CCADDEEBBA") == 280
         assert checkout_solution.checkout("AAAAAEEBAAABB") == 455
         assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 665
+
