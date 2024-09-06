fceb74da7e7e3251d3adb90247f4825711932234
sbx_user1051 <sbx_user1051@169.254.47.141>
2024-09-06 15:20:41 +0000

    Fri Sep 06 15:20:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,8 +26,8 @@ def checkout(skus: str) -> int:
             sorted_special_offers = sorted(item_extra_item_offers, key=lambda x: -x[0])
             for offer_count, offer_item in sorted_special_offers:
                 free_item_count = temp_count // offer_count
-                number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count
-                item_counts[offer_item] = item_counts[offer_item] - free_item_count
+                number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count else 0
+                item_counts[offer_item] = number_of_free_item_to_pay_for
                 temp_count %= offer_count
 
 
@@ -72,6 +72,7 @@ def checkout(skus: str) -> int:
         total -= prices[free_item] * number_of_item_type_to_discount
 
     return total
+
 
 
 
