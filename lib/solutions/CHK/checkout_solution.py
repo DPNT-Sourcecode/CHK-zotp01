9e557497c0d3b27f8fd930caad93f53531a8b44a
sbx_user1051 <sbx_user1051@169.254.47.141>
2024-09-06 15:20:09 +0000

    Fri Sep 06 15:20:09 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,28 +15,38 @@ def checkout(skus: str) -> int:
         else:
             item_counts[sku] += 1
 
-    adjustments = defaultdict(int)
     for item, count in item_counts.items():
         item_extra_item_offers = extra_item_offers.get(item)
         item_offers = offers.get(item)
 
         # calculate the free items to be removed from the total after the financial offers
         if item_extra_item_offers:
+            temp_count = count
             # apply the best offer first and then work down
             sorted_special_offers = sorted(item_extra_item_offers, key=lambda x: -x[0])
             for offer_count, offer_item in sorted_special_offers:
-                free_item_count = count // offer_count
-                number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count else 0
-                adjustments[offer_item] = number_of_free_item_to_pay_for
-                count %= offer_count
+                free_item_count = temp_count // offer_count
+                number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count
+                item_counts[offer_item] = item_counts[offer_item] - free_item_count
+                temp_count %= offer_count
 
-    for item, adjusted_count in adjustments.items():
-        item_counts[item] = adjusted_count
+
 
     total = 0
+    free_items = defaultdict(int)
     for item, count in item_counts.items():
         item_extra_item_offers = extra_item_offers.get(item)
         item_offers = offers.get(item)
+
+        # calculate the free items to be removed from the total after the financial offers
+        if item_extra_item_offers:
+            temp_count = count
+            # apply the best offer first and then work down
+            sorted_special_offers = sorted(item_extra_item_offers, key=lambda x: -x[0])
+            for offer_count, offer_item in sorted_special_offers:
+                free_item_count = temp_count // offer_count
+                free_items[offer_item] += free_item_count
+                temp_count %= offer_count
 
         if item_offers:
             # apply the best offer first and then work down
@@ -46,10 +56,23 @@ def checkout(skus: str) -> int:
                 count %= offer_count
         total += count * prices[item]
 
+    # remove the free items from the total
+    for free_item, free_item_count in free_items.items():
+        number_of_item_type_ordered = item_counts[free_item]
+        number_of_item_type_to_discount = min(free_item_count, number_of_item_type_ordered)
+        # check if the items were at a discounted rate
+        item_offers = offers.get(free_item)
+        if item_offers:
+            # apply the best offer first and then work down
+            sorted_offers = sorted(item_offers, key=lambda x: -x[0])
+            for offer_count, offer_price in sorted_offers:
+                total -= (number_of_item_type_to_discount // offer_count) * offer_price
+                number_of_item_type_to_discount %= offer_count
+
+        total -= prices[free_item] * number_of_item_type_to_discount
+
     return total
 
-assert checkout("CCADDEEBBA") == 280
-print(checkout("CCADDEEBBA"))
 
 
 
