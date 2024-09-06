a9dbfceb77629ab5af9b1bd0e53eb3960464dd88
sbx_user1051 <sbx_user1051@169.254.47.141>
2024-09-06 15:07:10 +0000

    Fri Sep 06 15:07:10 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,7 +15,6 @@ def checkout(skus: str) -> int:
         else:
             item_counts[sku] += 1
 
-    adjustments = defaultdict(int)
     for item, count in item_counts.items():
         item_extra_item_offers = extra_item_offers.get(item)
         item_offers = offers.get(item)
@@ -27,11 +26,8 @@ def checkout(skus: str) -> int:
             for offer_count, offer_item in sorted_special_offers:
                 free_item_count = count // offer_count
                 number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count else 0
-                adjustments[offer_item] = number_of_free_item_to_pay_for
+                item_counts[offer_item] = number_of_free_item_to_pay_for
                 count %= offer_count
-
-    for item, adjusted_count in adjustments.items():
-        item_counts[item] = adjusted_count
 
     total = 0
     for item, count in item_counts.items():
@@ -50,3 +46,4 @@ def checkout(skus: str) -> int:
 
 assert checkout("CCADDEEBBA") == 280
 print(checkout("CCADDEEBBA"))
+
