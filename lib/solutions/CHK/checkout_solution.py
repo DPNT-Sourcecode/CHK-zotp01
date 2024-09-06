320b833d8d7ca72a9d9f2872b3d48be707072685
sbx_user1051 <sbx_user1051@169.254.47.141>
2024-09-06 15:21:38 +0000

    Fri Sep 06 15:21:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,23 +15,6 @@ def checkout(skus: str) -> int:
         else:
             item_counts[sku] += 1
 
-    for item, count in item_counts.items():
-        item_extra_item_offers = extra_item_offers.get(item)
-        item_offers = offers.get(item)
-
-        # calculate the free items to be removed from the total after the financial offers
-        if item_extra_item_offers:
-            temp_count = count
-            # apply the best offer first and then work down
-            sorted_special_offers = sorted(item_extra_item_offers, key=lambda x: -x[0])
-            for offer_count, offer_item in sorted_special_offers:
-                free_item_count = temp_count // offer_count
-                number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count else 0
-                item_counts[offer_item] = number_of_free_item_to_pay_for
-                temp_count %= offer_count
-
-
-
     total = 0
     free_items = defaultdict(int)
     for item, count in item_counts.items():
@@ -72,9 +55,3 @@ def checkout(skus: str) -> int:
         total -= prices[free_item] * number_of_item_type_to_discount
 
     return total
-
-
-
-
-
-
